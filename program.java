b25488467ff1bf7c731a1fd7fc9c60fd9f40ea91
MarwanAhmed <marwan4125881@gmail.com>
2021-12-16 14:26:58 +0200

    Update program.java

--- program.java
+++ program.java
@@ -1,5 +1,5 @@
 public class test {
     public static void main (String [] args) {
-        System.out.println("Hello My Name is Marwan");
+        System.out.println("Hello My Name is Marwan Ahmed Hassan");
     }
 }
